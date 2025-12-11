9018f5cc438502175e2b9ffdcb7bb317efab104a
gdlale <alek.godlewskI@gmail.com>
2025-12-11 18:59:49 +0100

    Create main.py

---
+++ projekt_semestralny/main.py
@@ -0,0 +1,16 @@
+from funkcje import *
+
+
+def start():
+	print(line)
+	print("Nazywasz się janusz, masz 50 lat.\n"
+		  "Podczas przepychania się w Biedronce o karpia na świątecznej promocji potknąłeś się o stojące na środku korytarza palety.\n"
+		  "Uderzyłeś głową w lodówkę przez co straciłeś przytomność.\n"
+		  "Wdychanie grzyba z pod cieknących lodówek spowodowało przeniesienie się Janusza do fantastycznego świata.\n"
+		  "Twoim zadaniem jest przedrzeć się przez halucynacje i wrócić do rzeczywistości.")
+	swiat_1()
+
+
+if __name__ == '__main__':
+	inv.append(mikstura_heal)
+	start()
